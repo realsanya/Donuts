f49a5540775db0a8a9bdbb2cf3f34aa67b9581fc
Morozova Aleksandra <sashamorozova01@mail.ru>
2020-10-15 13:14:48 +0300

    add some files

---
+++ PuffyDonut/src/main/java/servlets/DetailServlet.java
@@ -0,0 +1,19 @@
+package servlets;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class DetailServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.getRequestDispatcher("/detail.ftl").forward(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doGet(request, response);
+    }
+}
